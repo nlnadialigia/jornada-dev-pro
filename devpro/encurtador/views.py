eeb6d184f2cd7d7562ce3e08c8f303816c2bbfb5
Nadia <nlnadialigia@gmail.com>
2021-05-19 21:16:21 -0300

    feat: finish redirect

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -7,7 +7,14 @@ from devpro.encurtador.models import UrlRedirect
 
 
 def relatorios(requisicao, slug):
-    return render(requisicao, 'encurtador/relatorio.html')
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida
+    }
+    return render(requisicao, 'encurtador/relatorio.html', contexto)
+
 
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
